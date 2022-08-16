eeb7018b7d6fc76cca67fa0e00ae129df7bf2920
Pascal Burkhard <pascal.burkhard@gmail.com>
2022-08-16 14:59:05 +0200

    Add rni_add_page and rni_map_multi_select_list functions

--- R/pages.R
+++ R/pages.R
@@ -17,3 +17,21 @@ rni_get_page_properties <- function(id) {
 rni_page_properties <- function(page) {
   page$properties |> purrr::map_dfr(rni_to_md)
 }
+
+#' Insert a page into a database or page
+#'
+#' @param parent A list with type (`database_id` or `page_id`) and the corresponding id
+#' @param properties A list of properties
+#' @param ...
+#'
+#' @return Nothing
+#' @export
+rni_add_page <- function(parent, properties, ...) {
+  rni_api_client()$pages$create(
+    parent = parent,
+    properties = properties,
+    ...
+  )
+}
+
+
---
+++ R/utils.R
@@ -0,0 +1,9 @@
+#' Make a list for multi-select properties
+#'
+#' @param category A string
+#'
+#' @return A list
+#' @export
+rni_map_multi_select_list <- function(category) {
+  return(list("name" = category))
+}
